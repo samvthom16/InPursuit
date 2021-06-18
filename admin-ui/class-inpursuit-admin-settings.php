2f7755f437e79301d5b515d9d111ff10eb75f73f
jayvardhan <vardhan.jay@gmail.com>
2021-06-18 16:42:45 +0530

    updated settings

--- admin-ui/class-inpursuit-admin-settings.php
+++ admin-ui/class-inpursuit-admin-settings.php
@@ -53,42 +53,130 @@ class INPURSUIT_ADMIN_SETTINGS extends INPURSUIT_BASE {
 	public function settingsOptionsRegistration()
 	{
 
+		//register section
 
-		add_settings_section( 
-			'inpursuit_settings_email_section',
-			'', 
-			'', 
-			'inpursuit-settings'
-		);
-
-
-		register_setting(
-			'inpursuit-settings',
-			'inpursuit_settings_bday_template',
-	        [
-	            'type' => 'string',
-	            'sanitize_callback' => 'sanitize_textarea_field',
-	            'default' => ''
-	        ]
-		);
-
-
-		add_settings_field( 
-			'inpursuit_settings_bday_template', 
-			'Birthday Email Template', 
-			[$this, 'birthdayEmailTemplateFieldCallback'], 
-			'inpursuit-settings', 
-			'inpursuit_settings_email_section',
+		$section_args = [
 			[
-			 	'label_for' => 'inpursuit_settings_bday_template',
+				'section-id' 	=> 'inpursuit_settings_email_section',
+				'section-title' => '',
+				'section-callback' => '',
+				'page-slug'		=> 'inpursuit-settings'
 			],
-		);
+		];
+
+		foreach ($section_args as $key => $option) {
+			$this->registerSection($option);
+		}
+
+
+		//register setting
+
+		$setting_args = [
+			[
+				'page-slug' 	=> 'inpursuit-settings',
+				'setting-name' 	=> 'inpursuit_settings_bday_template',
+			    'type-args' =>  [
+					             'type' => 'string',
+					             'sanitize_callback' => 'sanitize_textarea_field',
+					             'default' => ''
+					        	] 
+			],
+
+			[
+				'page-slug' 	=> 'inpursuit-settings',
+				'setting-name' 	=> 'inpursuit_settings_marriage_template',
+			    'type-args' =>  [
+					             'type' => 'string',
+					             'sanitize_callback' => 'sanitize_textarea_field',
+					             'default' => ''
+					        	] 
+			],
+		];
+
+		foreach ($setting_args as $key => $option) {
+			$this->registerSetting($option);
+		}
+
+		
+		//register setting field
+
+		$settings_fields_args = [
+			[
+				'setting-name' => 'inpursuit_settings_bday_template', 
+				'field-title'  => 'Birthday Email Template', 
+				'field-callback' =>	[$this, 'birthdayEmailTemplateFieldCb'], 
+				'page-slug'	   => 'inpursuit-settings', 
+				'section-id'   => 'inpursuit_settings_email_section',
+				'field-args' => [
+							 	 'label_for' => 'inpursuit_settings_bday_template',
+							    ],	
+			],
+
+			[
+				'setting-name' => 'inpursuit_settings_marriage_template', 
+				'field-title'  => 'Marriage Anniversary Email Template', 
+				'field-callback' =>	[$this, 'marriageEmailTemplateFieldCb'], 
+				'page-slug'	   => 'inpursuit-settings', 
+				'section-id'   => 'inpursuit_settings_email_section',
+				'field-args' => [
+							 	 'label_for' => 'inpursuit_settings_marriage_template',
+							    ],	
+			],
+		];
+
+		foreach ($settings_fields_args as $key => $option) {
+			$this->registerSettingField($option);
+		}
+
 
 	}
 
 
+	public function registerSection($arg)
+	{
+		add_settings_section(
+			$arg['section-id'], 
+			$arg['section-title'], 
+			$arg['section-callback'], 
+			$arg['page-slug']
+		);
+	}
 
-	public function birthdayEmailTemplateFieldCallback($args)
+
+	public function registerSetting($arg)
+	{
+		register_setting(
+			$arg['page-slug'], 
+			$arg['setting-name'], 
+			$arg['type-args']
+		);
+	}
+
+	public function registerSettingField($arg)
+	{
+		add_settings_field(
+			$arg['setting-name'], 
+			$arg['field-title'], 
+			$arg['field-callback'], 
+			$arg['page-slug'], 
+			$arg['section-id'], 
+			$arg['field-args']
+		);
+	}
+
+
+
+	public function birthdayEmailTemplateFieldCb($args)
+	{ 
+		$options =  get_option($args['label_for']); ?>
+
+		<textarea name="<?php echo $args['label_for'];?>" class="large-text" rows="10"><?php echo isset($options) ? $options : '';  ?></textarea>
+			
+		<?php
+	}
+
+
+	public function marriageEmailTemplateFieldCb($args)
 	{ 
 		$options =  get_option($args['label_for']); ?>
 
