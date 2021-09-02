461eed11151c593930d04299eb5c9771a57845b2
Samuel Thomas <samvthom16@gmail.com>
2021-09-02 15:22:29 +0530

    Refactor email notifications FROM Debugging

--- lib/email-notifications/class-inpursuit-mailer.php
+++ lib/email-notifications/class-inpursuit-mailer.php
@@ -2,51 +2,43 @@
 
 class INPURSUIT_MAILER extends INPURSUIT_BASE {
 
-    public function __construct() {
+  public function __construct() {
+    add_action( 'inpursuit_cron_email', [ $this, 'cronEmailCb'], 10, 4 );
+  }
 
-        add_action( 'inpursuit_cron_email', [ $this, 'cronEmailCb'], 10, 4);
+  /*
+  *  action hook cb to be used in scheduling cron email events
+  */
+	function cronEmailCb( $to, $subject, $body, $header ){ wp_mail( $to, $subject, $body, $header ); }
 
-    }
-
-
-    /**
-     *  action hook cb to be used in scheduling cron email events
-     * */
-	function cronEmailCb($to, $subject, $body, $header){
-		wp_mail($to, $subject, $body, $header);
-	}
-
-
-	/**
-     * Use this function when asyncronus wp_mail behaviour is required
-     * Schedules each outgoing email as a cron job
-     */
+  /*
+  * Use this function when asyncronus wp_mail behaviour is required
+  * Schedules each outgoing email as a cron job
+  */
 	function sendEmail( $to, $subject, $body, $header = array() ){
 
     // DEFAULT HEADER INFORMATION
     if( count( $header ) == 0 ){
-
       $from_name = get_option( 'inpursuit_settings_email_from_name' );
       $from_mail = get_option( 'inpursuit_settings_email_from_address' );
 
       $header = array(
         'Content-Type: text/html; charset=ISO-8859-1',
-        "$from_name <$from_mail>"
+        "From: $from_name <$from_mail>"
       );
     }
 
 		$args = array(
-				'to'		=> $to,
-				'subject'	=> $subject,
-				'body'		=> $body,
-				'header'	=> $header
-			);
+      'to'		  => $to,
+      'subject'	=> $subject,
+      'body'		=> $body,
+      'header'	=> $header
+    );
 
 		if(!wp_next_scheduled( 'inpursuit_cron_email' )){
 			wp_schedule_single_event( time(), 'inpursuit_cron_email', $args );
 		}
 	}
-
 }
 
 INPURSUIT_MAILER::getInstance();
--- lib/email-notifications/templates/comment-email.php
+++ lib/email-notifications/templates/comment-email.php
@@ -1,5 +1,6 @@
 <p><?php echo ucwords( $commentor_name )." has commented on ".ucwords($member_name)."'s profile";?></p>
-<p>Comment:</p>
-<div class="comment-body">
+<p>
+  <b>Comment:</b>
+  <br>
   <?php echo $comment_body; ?>
-</div>
+</p>
