9954cad155b1ad0ad08e497349c1afe4723a24e0
Suhail Bin Abdullah <suhail@mishantech.com>
2024-11-20 16:26:23 +0530

    rectified the access_field & pulled the proper limit_access groups from db

--- rest-api/class-inpursuit-rest-user.php
+++ rest-api/class-inpursuit-rest-user.php
@@ -13,39 +13,25 @@ class INPURSUIT_REST_USER extends INPURSUIT_BASE{
                 'update_callback' => array( $this, 'update_limit_access_field' )
             )
         );
-
-        register_rest_field('user', 'inpursuit_group_terms', [
-            'get_callback'    => array($this, 'get_inpursuit_group_terms_for_user'),
-        ]);
     }
 
     function get_limit_access_field($user, $field_name, $request) {
-        $value = get_user_meta($user['id'], $field_name, true);
-        return !empty($value) ? $value : [];
+        $user_db = INPURSUIT_DB_USER::getInstance();
+
+        $selected_groups = $user_db->getLimitedGroups( $user['id'] );
+        return $selected_groups;
     }
+    
     
     function update_limit_access_field($value, $user, $field_name) {
-        if (is_array($value)) {
-            update_user_meta($user->ID, $field_name, $value);
-        }
-        return true;
-    }
 
-    function get_inpursuit_group_terms_for_user($user) {
-        $terms = get_terms([
-            'taxonomy' => 'inpursuit-group',
-            'hide_empty' => false,
-        ]);
-    
-        $term_data = [];
-        foreach ($terms as $term) {
-            $term_data[] = [
-                'id' => $term->term_id,
-                'name' => $term->name,
-            ];
-        }
-    
-        return $term_data;
+        $user_db = INPURSUIT_DB_USER::getInstance();
+		$taxonomy = $user_db->getTaxonomy();
+
+		if( is_array( $value ) ){
+			return update_user_meta( $user->ID, $taxonomy, $value );
+		}
+        return false;
     }
 }
 
