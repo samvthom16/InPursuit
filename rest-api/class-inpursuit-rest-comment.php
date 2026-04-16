836625b248e472f7b599954a24a3775d99ff1550
Steve <anil.stephen66@gmail.com>
2026-04-16 11:37:04 +0530

    Fix: comment-filtering by category

--- rest-api/class-inpursuit-rest-comment.php
+++ rest-api/class-inpursuit-rest-comment.php
@@ -90,7 +90,7 @@ class INPURSUIT_REST_COMMENTS extends WP_REST_Controller {
 
       // FILTER COMMENTS BY CATEGORY_IDS
       if( $categories ){
-        $comment_ids = INPURSUIT_DB_COMMENTS_CATEGORY_RELATION::getInstance()->get_comment_ids_by_category_ids( $categories );
+        $comment_ids = INPURSUIT_DB_COMMENTS_CATEGORY_RELATION::getInstance()->get_comment_ids_by_category_ids( implode(",", $categories) );
         $params['comment_ids'] = $comment_ids ? $comment_ids : array(0);
   		}
 
